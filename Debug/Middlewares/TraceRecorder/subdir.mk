84dedbffddde3d9998803a0a4be4fc4468fdb4df
tomas <tomasblanchard@hotmail.fr>
2021-10-20 08:57:31 +0200

    Sémaphores 4.1.3

---
+++ Debug/Middlewares/TraceRecorder/subdir.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/TraceRecorder/trcInternalBuffer.c \
+../Middlewares/TraceRecorder/trcKernelPort.c \
+../Middlewares/TraceRecorder/trcSnapshotRecorder.c \
+../Middlewares/TraceRecorder/trcStreamingRecorder.c 
+
+OBJS += \
+./Middlewares/TraceRecorder/trcInternalBuffer.o \
+./Middlewares/TraceRecorder/trcKernelPort.o \
+./Middlewares/TraceRecorder/trcSnapshotRecorder.o \
+./Middlewares/TraceRecorder/trcStreamingRecorder.o 
+
+C_DEPS += \
+./Middlewares/TraceRecorder/trcInternalBuffer.d \
+./Middlewares/TraceRecorder/trcKernelPort.d \
+./Middlewares/TraceRecorder/trcSnapshotRecorder.d \
+./Middlewares/TraceRecorder/trcStreamingRecorder.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/TraceRecorder/%.o: ../Middlewares/TraceRecorder/%.c Middlewares/TraceRecorder/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F446xx -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"C:/Users/user/STM32CubeIDE/workspace_1.4.0/HELLO_FROM_START/Middlewares/TraceRecorder/config" -I"C:/Users/user/STM32CubeIDE/workspace_1.4.0/HELLO_FROM_START/Middlewares/TraceRecorder/include" -I"C:/Users/user/STM32CubeIDE/workspace_1.4.0/HELLO_FROM_START/Middlewares/TraceRecorder/extras/SDK/include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
